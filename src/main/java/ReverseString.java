333c0b470082b49ebb1b8c029487995fd68b115f
vvaidya1 <vaibhav.vaidya11@gmail.com>
2021-12-23 21:39:43 +0000

    Leetcode solutions java

---
+++ src/main/java/ReverseString.java
@@ -0,0 +1,16 @@
+class ReverseString {
+    public void reverseString(char[] s) {
+        int start = 0, end = s.length - 1;
+        while (start < end) {
+            swap(s, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    void swap(char[] array, int i, int j) {
+        char ch = array[i];
+        array[i] = array[j];
+        array[j] = ch;
+    }
+}
