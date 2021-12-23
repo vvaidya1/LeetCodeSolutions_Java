22bf9def554b4663989b1a120520a8d4a071b927
vvaidya1 <vaibhav.vaidya11@gmail.com>
2021-12-23 22:10:55 +0000

    Leetcode solutions java

---
+++ src/main/java/ReverseVowelsOfAString.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReverseVowelsOfAString {
+    Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+    public String reverseVowels(String s) {
+        int start = 0, end = s.length() - 1;
+        char[] strArray = s.toCharArray();
+        while (start < end) {
+            if (isVowel(strArray[start]) && isVowel(strArray[end])) {
+                swap(strArray, start, end);
+                start++;
+                end--;
+            } else if (!isVowel(strArray[start])) start++;
+            else end--;
+        }
+        return String.valueOf(strArray);
+    }
+
+    void swap(char[] strArray, int i, int j) {
+        char ch = strArray[i];
+        strArray[i] = strArray[j];
+        strArray[j] = ch;
+    }
+
+    boolean isVowel(char ch) {
+        return vowels.contains(ch);
+    }
+}
