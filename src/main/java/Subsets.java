43e5f6fd1e30ec2ea37f53455d0445d571d4bcd9
vvaidya1 <vaibhav.vaidya11@gmail.com>
2021-12-24 20:09:42 +0000

    Leetcode solutions java

---
+++ src/main/java/Subsets.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/*
+ * https://leetcode.com/problems/subsets/
+ * O(n.2^n) time complexity | O(n.2^n) space complexity
+ */
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        if (nums.length == 0) return new ArrayList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for (int num : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> newList = new ArrayList<>(result.get(i));
+                newList.add(num);
+                result.add(newList);
+            }
+        }
+        return result;
+    }
+}
