d9666b9e8c01787e6471bce69dc486f9f5ef4c7a
vvaidya1 <vaibhav.vaidya11@gmail.com>
2021-12-27 20:24:29 +0000

    Leetcode solutions java

---
+++ src/main/java/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,18 @@
+/*
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+ * O(n) time | O(1) space
+ */
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - prices[i-1] > 0) {
+                maxProfit += prices[i] - prices[i-1];
+            }
+        }
+
+        return maxProfit;
+    }
+}
