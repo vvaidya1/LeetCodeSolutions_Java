6660f7adb6e09557a6ff411e159a594663efb674
vvaidya1 <vaibhav.vaidya11@gmail.com>
2022-01-02 03:25:06 +0000

    Leetcode solutions java

---
+++ src/main/java/ClimbStairs.java
@@ -0,0 +1,16 @@
+/*
+ * https://leetcode.com/problems/climbing-stairs/
+ * O(n) time | O(n) space
+ */
+public class ClimbStairs {
+    public int climbStairs(int n) {
+        int[] ways = new int[n+1];
+        ways[0] = 1;
+        ways[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            ways[i] = ways[i-1] + ways[i-2];
+        }
+        return ways[n];
+    }
+}
