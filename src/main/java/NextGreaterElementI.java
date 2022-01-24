ec975b5ef6c851731c7fbf2a1e3ace168d3f0e72
vvaidya1 <vaibhav.vaidya11@gmail.com>
2022-01-24 23:43:24 +0000

    Leetcode solutions java

---
+++ src/main/java/NextGreaterElementI.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/*
+ * https://leetcode.com/problems/next-greater-element-i/
+ * O(n) time | O(n) space
+ * Reference https://www.youtube.com/watch?v=Du881K7Jtk8&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=75
+ */
+public class NextGreaterElementI {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreaterMap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
+        }
+        return result;
+    }
+}
