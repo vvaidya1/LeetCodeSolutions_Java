f9e8040f33f98a603db3982c043ac912cf3c5f4c
vvaidya1 <vaibhav.vaidya11@gmail.com>
2022-01-28 18:38:04 +0000

    Leetcode solutions java

---
+++ src/main/java/LongestPallindromicSubstring.java
@@ -0,0 +1,30 @@
+
+/*
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ * O(n^2) time | O(1) space
+ */
+public class LongestPallindromicSubstring {
+    public String longestPalindrome(String s) {
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandFromCenter(s, i, i);
+            int len2 = expandFromCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    int expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
