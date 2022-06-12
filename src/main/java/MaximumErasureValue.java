680a75a63e74c222c0603a2140609055380578f3
vvaidya1 <vaibhav.vaidya11@gmail.com>
2022-06-12 17:38:24 +0100

    Leetcode solutions java

--- src/main/java/MaximumErasureValue.java
+++ src/main/java/MaximumErasureValue.java
@@ -9,16 +9,17 @@ public class MaximumErasureValue {
         if (nums == null || nums.length == 0)
             return 0;
 
-        int result = 0;
-        Map<Integer, Integer> tracker = new HashMap<>();
         int[] prefixSum = new int[nums.length + 1];
-        for (int l = 0, r = 0; r < nums.length; r++) {
-            prefixSum[r + 1] = prefixSum[r]+ nums[r];
+        Map<Integer, Integer> tracker = new HashMap<>();
+        int result = 0;
+        for (int left = 0, right = 0; right < nums.length; right++) {
+            prefixSum[right + 1] = prefixSum[right] + nums[right];
 
-            if (tracker.containsKey(nums[r]))
-                l = Math.max(l, tracker.get(nums[r]) + 1);
-            result = Math.max(result, prefixSum[r + 1] - prefixSum[l]);
-            tracker.put(nums[r], r);
+            if (tracker.containsKey(nums[right]))
+                left = Math.max(left, tracker.get(nums[right]) + 1);
+
+            result = Math.max(result, prefixSum[right + 1] - prefixSum[left]);
+            tracker.put(nums[right], right);
         }
         return result;
     }
