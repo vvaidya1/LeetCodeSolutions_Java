1562099ecf6ce09ee6a8243696a4fcc4ee99812f
vvaidya1 <vaibhav.vaidya11@gmail.com>
2022-07-02 12:14:36 +0100

    Leetcode solutions java

---
+++ src/main/java/MaximumAreaOfAPieceOfCake.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/
+ * O(n.log.n) time | O(1) space - where n is the maximum of lengths of horizontal and vertical cuts
+ */
+public class MaximumAreaOfAPieceOfCake {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        long mod = (long)1e9+7;
+
+        long maxH = horizontalCuts[0], maxW = verticalCuts[0], prevH = 0, prevW = 0;
+        for (int hCut : horizontalCuts) {
+            maxH = Math.max(maxH, hCut - prevH);
+            prevH = hCut;
+        }
+        maxH = Math.max(maxH, h - prevH);
+
+        for (int vCut : verticalCuts) {
+            maxW = Math.max(maxH, vCut - prevW);
+            prevW = vCut;
+        }
+        maxW = Math.max(maxW, w - prevW);
+
+        return (int)((maxH * maxW) % mod);
+    }
+}
